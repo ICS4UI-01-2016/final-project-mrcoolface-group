a494d8df4debd7df27a21d553becf69f5f912a25
Josh <Josh@DESKTOP-N1045J1>
2016-12-19 12:43:32 -0500

    Commit

---
+++ desktop/src/com/slide/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slide.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slide.game.Slide;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Slide(), config);
+	}
+}
